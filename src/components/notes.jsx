59d77ea66591c5f2301a737b1fb64248f11a7cda
Yuvraj Jaiswal <yuvraj.jaiswal@erasmith.com>
2024-08-01 03:12:18 +0530

    first commit

---
+++ src/components/notes.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./notes.css";
+import { Button } from "react-bootstrap";
+
+const Notes = ({ notes, markAsComplete, markAsImportant, removeNote }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredNotes = searchQuery
+    ? notes.filter((note) =>
+        note.title.toLowerCase().includes(searchQuery.toString().toLowerCase())
+      )
+    : notes;
+
+  return (
+    <div>
+      <h1 className="Notes-Heading-top">Notes</h1>
+      <input
+        className="notes-input"
+        type="text"
+        placeholder="Search by title..."
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      {filteredNotes.map((note, index) => (
+        <div className="note-container" key={index}>
+          <h3>Title</h3>
+          <h5
+            className={`note ${note.complete ? "complete" : ""} ${
+              note.important ? "important" : ""
+            }`}
+          >
+            {note.title}
+          </h5>
+          <h3>Summary</h3>
+          <p>{note.summary}</p>
+          <Button
+            className="Notes-btn"
+            variant="success"
+            onClick={() => markAsComplete(index)}
+          >
+            Mark as Complete
+          </Button>
+          <Button
+            className="Notes-btn"
+            variant="dark"
+            onClick={() => markAsImportant(index)}
+          >
+            Mark as Important
+          </Button>
+          <Button
+            className="Notes-btn"
+            variant="danger"
+            onClick={() => removeNote(index)}
+          >
+            Remove
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Notes;
